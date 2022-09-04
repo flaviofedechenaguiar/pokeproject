54e73c47f27370c15da943edc2dd013ba85c8d39
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-04 12:07:45 -0300

    feat: add ChakraProvider with theme and favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+
+import theme from '../../theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
